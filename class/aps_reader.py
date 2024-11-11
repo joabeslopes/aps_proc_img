7e03aa9406e57bddab4c234aa704d86cb184e0dd
joabe <joabe@notebook>
2024-11-11 15:27:45 -0300

    correcao roi

--- class/aps_reader.py
+++ class/aps_reader.py
@@ -18,14 +18,15 @@ class PlacaDetector:
         """Processa a imagem, detectando placas e usando OCR."""
         cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         placas = self.placa_cascade.detectMultiScale(cinza, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-        roi_placa = None
+        # Se for uma tupla é porque não detectou o padrão
+        if isinstance(placas, tuple):
+            return img
+
         for (x, y, w, h) in placas:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi_placa = img[y:y + h, x:x + w]
-        if roi_placa.any():
-            return roi_placa
-        else:
-            return img
+
+        return roi_placa
 
     def executar(self):
         """Carrega a imagem e realiza a detecção de placas."""
@@ -43,9 +44,9 @@ class PlacaDetector:
             self.array_placas.append(n[1])
             print(f"possivel placa lida: {n[1]}")
 
-        # Exibir a imagem com as placas detectadas
-        cv2.imshow('Deteccao de Placas', img_placa)
+        # # Exibir a imagem com as placas detectadas
+        # cv2.imshow('Deteccao de Placas', img_placa)
 
-        # Aguardar até que o usuário feche a janela
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        # # Aguardar até que o usuário feche a janela
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
