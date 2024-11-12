f17037f4bc356f023cd56b3c9b45674e03dc5019
joabe <joabe@notebook>
2024-11-12 16:52:22 -0300

    ajustes DDL

--- db/init.sql
+++ db/init.sql
@@ -1,16 +1,28 @@
 USE aps_database;
 
-CREATE TABLE aps_veiculos (
-id INT AUTO_INCREMENT,
-placa CHAR(7) NOT NULL,
-marca VARCHAR(30),
-modelo VARCHAR(30),
+CREATE TABLE aps_moradores (
+    id INT AUTO_INCREMENT,
+    nome VARCHAR(30) NOT NULL,
 
-primary key(id)
+    primary key(id)
 );
 
+CREATE TABLE aps_veiculos (
+id INT AUTO_INCREMENT,
+placa CHAR(7) NOT NULL UNIQUE,
+marca VARCHAR(30),
+id_morador INT NOT NULL,
 
-INSERT INTO aps_veiculos(placa,marca,modelo) VALUES
-('CSA5J19','VOLKSWAGEN','PARATI'),
-('FJY2E56','VOLKSWAGEN','POLO TRACK'),
-('FOL7B05','CHEVROLET','COBALT');
+primary key(id),
+constraint fk_morador foreign key(id_morador) references aps_moradores(id)
+);
+
+INSERT INTO aps_moradores(id, nome) VALUES
+(1, 'João'),
+(2, 'Marcos'),
+(3, 'Rafael');
+
+INSERT INTO aps_veiculos(placa,marca,id_morador) VALUES
+('GIK4130','FORD',1),
+('GIS3F45','VOLKSWAGEN',2),
+('EVG9F80','HYUNDAI',3);
